6b072029fbdd619ccef2ff09fc2a5991995c2864
rkpeterson <petersrk@email.sc.edu>
2012-11-11 22:23:32 -0500

    Added Voce Library Main page: http://voce.sourceforge.net/ Java API: http://voce.sourceforge.net/api/java/classvoce_1_1_speech_interface.html#e0
    Threw in a little number recognition and resynthesis example in
    TestRun.java
    
    All libraries and dependencies should be included

--- src/TestRun.java
+++ src/TestRun.java
@@ -7,7 +7,24 @@ public class TestRun {
 	public static void main(String[] args){
 		System.out.println("Welcome");
 		new MainFrame();
-		
+		voce.SpeechInterface.init("voce-0.9.1/lib", true, true, "./gram", "digits");
+		System.out.println("speak digits or quit");
+		boolean quit = false;
+		while (!quit) {
+			try {
+				Thread.sleep(200);
+			}
+			catch (InterruptedException e) {}
+			while (voce.SpeechInterface.getRecognizerQueueSize() > 0) {
+				String s = voce.SpeechInterface.popRecognizedString();
+				if (-1 != s.indexOf("quit")) {
+					quit = true;
+				}
+				System.out.println("You said: " + s);
+				voce.SpeechInterface.synthesize(s);
+			}
+		}
+		voce.SpeechInterface.destroy();
 	}
 
 }
